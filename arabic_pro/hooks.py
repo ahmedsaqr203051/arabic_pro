369d7cfab8b5fddde12df2605b97e344564d45e6
Ahmed_Saqr2050 <ahmedsaqr2071@gmail.com>
2025-12-26 19:30:31 +0300

    ar

--- arabic_pro/hooks.py
+++ arabic_pro/hooks.py
@@ -10,7 +10,7 @@ app_license = "mit"
 
 # include js, css files in header of desk.html
 app_include_css = "/assets/arabic_pro/css/arabic_pro.bundle.css"
-# app_include_js = "/assets/arabic_pro/js/arabic_pro.js"
+app_include_js = "/assets/arabic_pro/js/arabic_pro.bundle.js"
 
 # include js, css files in header of web.html
 # web_include_css = "/assets/arabic_pro/css/arabic_pro.css"
---
+++ arabic_pro/public/js/arabic_pro.bundle.js
@@ -0,0 +1,1 @@
+console.log("Arabic Pro loaded");
